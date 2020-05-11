96977f63a15a779f78df953273a0632c09c53bc8
miriam-sullivan <maljayd42@gmail.com>
2020-05-11 16:38:33 -0500

    added in everything now to troubleshoot

---
+++ accounts/accounts-router.js
@@ -0,0 +1,91 @@
+const express = require("express");
+
+//knex
+const db = require("../data/dbConfig.js");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  db.select("*")
+    .from("accounts")
+    .then((accounts) => {
+      res.status(200).json({ data: accounts });
+    })
+    .catch((errror) => {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .first()
+    .then((account) => {
+      if (account) {
+        res.status(200).json({ data: account });
+      } else {
+        res.status(404).json({ message: "No accounts found with that id" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    });
+});
+
+router.post("/", (req, res) => {
+  const account = req.body;
+  if (isValidAccount(account)) {
+    db("accounts")
+      .insert(account, "id")
+      .then((ids) => {
+        res.status(201).json({ data: ids });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+      });
+  }
+});
+
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  db("accounts")
+    .where({ id: req.params.id })
+    .update(changes)
+    .then((countOfRecords) => {
+      if (countOfRecords > 0) {
+        res.status(200).json({ data: count });
+      } else {
+        res.status(404).json({ message: "record not found by that id" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  db("post")
+    .where({ id: req.params.id })
+    .del()
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ data: count });
+      } else {
+        res.status(404).json({ message: "record not found by that id" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    });
+});
+
+function isValidAccount(account) {
+  return Boolean(account.name && account.budget);
+}
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
 const db = require("../data/dbConfig.js");
-
+const AccountRouter = require("../accounts/accounts-router.js");
 const server = express();
 
 server.use(express.json());
-
+server.use("api/accounts", AccountRouter);
 module.exports = server;
